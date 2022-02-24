19bd9fd0a77f1831af2663561930953586caba14
BobbySlope <info@hoobs.org>
2022-02-24 21:42:32 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -58,6 +58,18 @@ echo " "
 echo "install Fullscreen Dashboard...."
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
 sudo apt-get install --no-install-recommends chromium-browser -y
+echo "set screen...."
+
+sudo rm -rf usr/share/X11/xorg.conf.d/99-fbturbo.conf
+cat > /usr/share/X11/xorg.conf.d/99-fbturbo.conf <<EOL
+Section "Device"
+        Identifier      "Allwinner A10/A13/A20 FBDEV"
+        Driver          "fbturbo"
+        Option          "fbdev" "/dev/fb1"
+
+        Option          "SwapbuffersWait" "true"
+EndSection
+EOL
 
 echo "set config...."
 cat > /etc/X11/xorg.conf <<EOL
