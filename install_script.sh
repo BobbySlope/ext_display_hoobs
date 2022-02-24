05a9caa844244644ccd702bf3921c5106baeb2f7
BobbySlope <info@hoobs.org>
2022-02-24 21:57:46 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -56,10 +56,10 @@ echo "Autologin CLI Installed"
 echo "----------------------------------------------------------------"
 echo " "
 echo "install Fullscreen Dashboard...."
-sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
+sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit xserver-xorg-video-fbdev openbox -y
 sudo apt-get install --no-install-recommends chromium-browser -y
-echo "set screen...."
 
+echo "set screen...."
 sudo rm -rf usr/share/X11/xorg.conf.d/99-fbturbo.conf
 cat > /usr/share/X11/xorg.conf.d/99-fbturbo.conf <<EOL
 Section "Device"
