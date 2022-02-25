7df8d4723a5cd6cc57566709a97a80429f2191f3
BobbySlope <info@hoobs.org>
2022-02-25 21:35:26 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -59,6 +59,7 @@ echo " "
 echo "install Fullscreen Dashboard...."
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit xserver-xorg-video-fbdev openbox -y
 sudo apt-get install --no-install-recommends chromium-browser -y
+sudo apt-get install lightdm -y
 
 #echo "set screen...."
 #sudo rm -rf usr/share/X11/xorg.conf.d/99-fbturbo.conf
