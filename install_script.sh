6d614ce6a1996b6379bf63dcb77f1a1e060bd23d
BobbySlope <info@hoobs.org>
2022-02-25 21:39:15 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -87,8 +87,6 @@ cat > /opt/kiosk.sh <<EOL
 xset dpms
 xset s noblank
 xset s 300
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 openbox-session &
 chromium --kiosk --incognito http://localhost
 EOL
