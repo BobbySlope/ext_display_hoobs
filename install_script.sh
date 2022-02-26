5079dd3005ca58f3a487e0f671da4a29a2aa4d70
BobbySlope <info@hoobs.org>
2022-02-26 08:00:59 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -30,32 +30,6 @@
 
 
 sudo apt-get update --yes
-#echo "----------------------------------------------------------------"
-#echo "This script will Setup the external Touchdisplay Widget for HOOBS"
-#echo "----------------------------------------------------------------"
-
-#echo "Setup Touchscreen...."
-#sudo rm -rf LCD-show
-#git clone https://github.com/goodtft/LCD-show.git
-#sudo chmod -R 755 LCD-show
-#cd LCD-show/
-#sudo ./LCD35-show
-#echo "----------------------------------------------------------------"
-#echo "Touchscreen Installed"
-#echo "----------------------------------------------------------------"
-#echo "set screen...."
-#sudo rm -rf usr/share/X11/xorg.conf.d/99-fbturbo.conf
-#cat > /usr/share/X11/xorg.conf.d/99-fbturbo.conf <<EOL
-#Section "Device"
-#        Identifier      "Allwinner A10/A13/A20 FBDEV"
-#        Driver          "fbturbo"
-#        Option          "fbdev" "/dev/fb1"
-
-#        Option          "SwapbuffersWait" "true"
-#EndSection
-EOL
-
-
 echo "Setup Autologin to CLI...."
 sudo mkdir /lib/systemd/system/getty@tty1.service.d/
 cat > /lib/systemd/system/getty@tty1.service.d/20-autologin.conf <<EOL
@@ -66,7 +40,7 @@ ExecStart=-/sbin/agetty --autologin hoobs --noclear %I $TERM
 EOL
  
 echo "install Fullscreen Dashboard...."
-sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit xserver-xorg-video-fbdev openbox -y
+DEBIAN_FRONTEND=noninteractive sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit xserver-xorg-video-fbdev openbox -y
 sudo apt-get install firefox-esr -y
 
 echo "set openbox to start firefox with url...."
